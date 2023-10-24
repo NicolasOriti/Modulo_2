a8bc73816fb789a0f35f315f2c244ea62984bdae
Nicolas Oriti Tizio <oritinicolas@gmail.com>
2023-10-23 21:18:24 -0300

    feat(clase2): operators and types

---
+++ clases/01-variables.js
@@ -0,0 +1,60 @@
+// alert("Hola Mundo")
+// Alert
+// ALERT
+// aLErt
+/* 
+  Esto es
+  un comentario
+  multilinea
+*/
+
+// VARIABLES
+// Es un espacio en memoria donde se guardan dates que se puede reutilizar
+
+// Declaracion e inicializacion de una variable
+let nombre = 'Nicolas'; // let nos permite declarar una variable para el ambito donde se use
+var apellido = 'Oriti Tizio'; // var declara una variable siempre GLOBAL sin importar donde se use
+const PI = 3.14; // const define una variable que no puede modificarse (CONSTANTE)
+
+// Tipos de datos
+
+// PRIMITIVOS:
+
+// String || Cadena de texto
+let vendedor = 'Enrico';
+let vendedor2 = 'Gonzalo';
+console.log('Vendedor: ', vendedor);
+
+// Number || Numero
+let num1 = 23;
+let num2 = 10;
+console.log('num1: ', num1);
+console.log('num2: ', num2);
+
+// Boolean || Buleanos
+let verdadero = true;
+let falso = false;
+console.log('verdadero: ', verdadero);
+console.log('falso: ', falso);
+
+// Null || Nulo
+let dinero = null;
+console.log('dinero: ', dinero);
+
+// Undefined || Indefinido
+let sueldo;
+console.log('sueldo: ', sueldo);
+
+// COMPLEJOS
+
+// Array || Arreglo
+let nombreVariable = [];
+let arreglo = [1, 2, 3, 4, 5];
+
+// Object || Objeto
+let persona = {
+  nombre: 'Nicolas',
+  apellido: 'Oriti Tizio',
+  edad: 23,
+  ciudadNatal: 'Rosario',
+};
---
+++ clases/02-operadores.js
@@ -0,0 +1,81 @@
+// INT = integer = entero
+let num1 = Number(prompt('Ingrese el primer numero: '));
+let num2 = prompt('Ingrese el segundo numero: ');
+let num3 = 5;
+console.log('num1: ', num1);
+console.log('num2: ', num2);
+
+// 1- Operadores de asignacion
+
+// num3 += 5; // num1 = num1 + 5;
+// num3 -= 5; // num1 = num1 - 5;
+// num3 *= 5; // num1 = num1 * 5;
+// num3 /= 5; // num1 = num1 / 5;
+// num3 %= 5; // num1 = num1 % 5;
+
+// // 2- Operadores aritmeticos
+
+// // Suma
+// let suma = num1 + num2;
+// console.log('Suma = ', suma);
+
+// // Resta
+// let resta = num1 - num2;
+// console.log('Resta = ', resta);
+
+// // Multiplicacion
+// let multiplicacion = num1 * num2;
+// console.log('Multiplicacion = ', multiplicacion);
+
+// // Division
+// let division = num1 / num2;
+// console.log('Division = ', division);
+
+// // Modulo || Resto
+// let resto = num1 % num2;
+// console.log('Resto = ', resto);
+
+// // 3- Operadores unitarios
+
+// // Incremento
+// // num1 = num1 + 1;
+// num1++;
+// console.log('Incremento: ', num1);
+
+// // Decremento
+// // num2 = num2 - 1;
+// num2--;
+// console.log('Decremento: ', num2);
+
+// let verdadero = !false;
+// let falso = !verdadero;
+
+// 4- Operadores relacionales
+
+console.log(num1 + ' y ' + num2 + ' Son iguales? ', num1 == num2);
+console.log(num1 + ' y ' + num2 + ' Son diferentes? ', num1 != num2);
+console.log(num1 + ' es mayor que ' + num2 + '? ', num1 > num2);
+console.log(num1 + ' es menor que ' + num2 + '? ', num1 < num2);
+console.log(num1 + ' es mayor o igual que ' + num2 + '? ', num1 >= num2);
+console.log(num1 + ' es menor o igual que ' + num2 + '? ', num1 <= num2);
+
+console.log(num1 + ' y ' + num2 + ' Son estrictamente iguales? ', num1 === num2);
+console.log(num1 + ' y ' + num2 + ' Son estrictamente diferentes? ', num1 !== num2);
+
+// 5- Operadores logicos
+
+// AND
+console.log('AND: ', true && true);
+console.log('AND: ', true && false);
+console.log('AND: ', false && true);
+console.log('AND: ', false && false);
+
+// OR
+console.log('OR: ', true || true);
+console.log('OR: ', true || false);
+console.log('OR: ', false || true);
+console.log('OR: ', false || false);
+
+// NOT 
+console.log('NOT: ', !true);
+console.log('NOT: ', !false);
