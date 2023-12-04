5a8a45184ce9e5fec39cff5ccc90b83a52491d4d
Nicolas Oriti Tizio <oritinicolas@gmail.com>
2023-12-04 20:41:07 -0300

    clase-13: localstorage y JSON

--- mini-proyectos/to-do/index.js
+++ mini-proyectos/to-do/index.js
@@ -1,16 +1,57 @@
-const form = document.querySelector('form')
-const root = document.querySelector('#root')
+const form = document.querySelector('form');
+const root = document.querySelector('#root');
+const input = document.querySelector('input');
+let dataList = [];
+
+if (localStorage.getItem('list')) {
+  dataList = JSON.parse(localStorage.getItem('list'));
+}
+
+dataList.forEach(element => {
+  root.innerHTML += `
+    <div class="card">
+      ${element}
+      <button onclick="">Eliminar</button>
+    </div>
+  `;
+});
 
 const handleOnSubmit = (event) => {
   event.preventDefault();
-  console.log(event.target[1])
-  const input = event.target[0]
+  dataList.push(input.value);
+  localStorage.setItem('list', JSON.stringify(dataList));
   root.innerHTML += `
     <div class="card">
       ${input.value}
     </div>
   `;
   event.target.reset();
+};
+const handleOnRemove = (event) => {
+  
 }
 
 form.addEventListener('submit', handleOnSubmit);
+
+// localStorage.clear();
+// localStorage.getItem('nombre');
+// localStorage.removeItem('nombre');
+
+// JSON.parse({
+//   "nombre": "Luis",
+//   "apellido": "Perez",
+//   "imagen": {
+//     "url": "http://www.google.com",
+//     "alt": "Imagen de Luis",
+//     "class": "img-responsive"
+//   }
+// })
+// JSON.stringify({
+//   nombre: 'Luis',
+//   apellido: 'Perez',
+//   imagen: {
+//     url: 'http://www.google.com',
+//     alt: 'Imagen de Luis',
+//     class: 'img-responsive',
+//   },
+// })
